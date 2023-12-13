7ec13873ce729aa9553ca4e4bcfc950461f032d2
NghiaNguyen090499 <114667528+NghiaNguyen090499@users.noreply.github.com>
2023-12-13 11:23:26 +0700

    Update settings.py

--- VoteApp/settings.py
+++ VoteApp/settings.py
@@ -69,28 +69,28 @@ TEMPLATES = [
 WSGI_APPLICATION = 'VoteApp.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
+Database
+https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Vote_App',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'127.0.0.1',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'Vote_App',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306',
+#     }
+# }
 
 
 # Password validation
