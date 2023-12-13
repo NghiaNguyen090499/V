ee2dad77541452ed6cd25b60df8e38714d90091f
NghiaNguyen090499 <114667528+NghiaNguyen090499@users.noreply.github.com>
2023-12-13 10:33:44 +0700

    update

--- noel/models.py
+++ noel/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+class SubCategory(models.Model):
+   
+    name = models.CharField(max_length=150, unique=True)
+    image = models.ImageField(upload_to='mycakes/images', default='mycakes/images/default.png')
+
+    def __str__(self):
+        return self.name
+    
+    
+
+class Product(models.Model):
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.PROTECT, related_name='products')
+    name = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='mycakes/images', default='mycakes/images/default.png')   
+    public_day = models.DateTimeField(default=timezone.now)
+    
+
+    def __str__(self):
+        return self.name
